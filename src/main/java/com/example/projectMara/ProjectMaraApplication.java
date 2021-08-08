007b400d8733db3928d9ad67a607a1e0550a30d5
Marcin <Marcin@KursGIT.pl>
2021-08-08 11:41:41 +0200

    initial commit

---
+++ src/main/java/com/example/projectMara/ProjectMaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectMara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectMara/ProjectMaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectMara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

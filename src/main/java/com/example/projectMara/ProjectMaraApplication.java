2029d052447cb826ea7c5bd8098bad2ff6d7756f
Marcin <Marcin@KursGIT.pl>
2021-08-21 13:37:48 +0200

    Movie manipulation

--- src/main/java/com/example/projectMara/ProjectMaraApplication.java
+++ src/main/java/com/example/projectMara/ProjectMaraApplication.java
@@ -1,5 +1,6 @@
 package com.example.projectMara;
 
+import com.example.projectMara.domain.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class ProjectMaraApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectMaraApplication.class, args);
 	}
-
 }

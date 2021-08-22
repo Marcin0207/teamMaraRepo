9769dd2ed8d8f5369b6c50659fe4ec701f26e985
Aleksandra Zaremba <ola.zaremba88@gmail.com>
2021-08-22 14:45:05 +0200

    Copies added to BD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,20 @@ INSERT INTO MOVIES (AVG_RATE,DESCRIPTION,DIRECTOR,MOVIE_GENRE,PREMIERE_DATE,TITL
   ('9.3','Niepełnosprawny weteran wciela się w obce ciało by poderwać niebieską kosmitkę','James Cameron','SCI_FI','2009-12-25','Avatar','CLASSIC', '2000-04-11 09:31:15'),
   ('7.8','Duch z depresją nawiedza dzieciaka swoimi problemami małrzeńskimi','M. Night Shaymalan','THRILLER','2000-01-14','Szósty zmysł','CLASSIC','2000-04-11 09:31:15'),
   ('6.7','Były pastor przetrzymuje w piwnicy imigranta cierpiącego na wodostręt','M. Night Shaymalan','THRILLER','2002-11-08','Znaki','CLASSIC','2000-04-11 09:31:15');
+
+INSERT INTO COPIES (MOVIE_ID, ORDER_ID) VALUES
+
+('1','1'),
+('2','1'),
+('3','1');
+
+INSERT INTO COPIES (MOVIE_ID) VALUES
+
+('1'),
+('1'),
+('2'),
+('2'),
+('2'),
+('3'),
+('3'),
+('2');

e91a65f5c27e2bfb1a999975d831e467567c949f
Aadm Klonowski <adamklonowski91@gmail.com>
2021-09-05 12:19:10 +0200

    @EnableScheduling in main app

--- src/main/java/com/example/projectMara/ProjectMaraApplication.java
+++ src/main/java/com/example/projectMara/ProjectMaraApplication.java
@@ -3,8 +3,11 @@ package com.example.projectMara;
 import com.example.projectMara.domain.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class ProjectMaraApplication {
 
 	public static void main(String[] args) {
